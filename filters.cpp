a9100b2e57eb3b7efe6b643258c66ed8663e53f1
arashzjahangiri <arash.jahangiri@snapp.cab>
2019-12-17 18:48:31 +0330

    initial commit

---
+++ filters.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include "opencv2/imgproc.hpp"
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+using namespace std;
+using namespace cv;
+int MAX_KERNEL_LENGTH = 31;
+int DELAY_BLUR = 100;
+int DELAY_CAPTION = 3000;
+Mat src; Mat dst;
+char window_name[] = "Smoothing Demo";
+
+int display_caption(const char *caption);
+int display_dst(int delay);
+
+int main(int argc, char **argv)
+{
+    namedWindow( window_name, WINDOW_AUTOSIZE );
+    const char* filename = argc >=2 ? argv[1] : "nature.jpg";
+    src = imread( samples::findFile( filename ), IMREAD_COLOR );
+    
+    if (src.empty())
+    {
+        printf(" Error opening image\n");
+        printf(" Usage:\n %s [image_name-- default lena.jpg] \n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    
+    if( display_caption( "Original Image" ) != 0 )
+    {
+        return 0;
+    }
+    
+    dst = src.clone();
+    
+    if( display_dst( DELAY_CAPTION ) != 0 )
+    {
+        return 0;
+    }
+    
+    if( display_caption( "Homogeneous Blur" ) != 0 )
+    {
+        return 0;
+    }
+    
+    for ( int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2 )
+    {
+        blur( src, dst, Size( i, i ), Point(-1,-1) );
+        if( display_dst( DELAY_BLUR ) != 0 )
+        {
+            return 0;
+        }
+    }
+    
+    if( display_caption( "   Blur" ) != 0 )
+    {
+        return 0;
+    }
+    
+    for ( int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2 )
+    {
+        GaussianBlur( src, dst, Size( i, i ), 0, 0 );
+        if( display_dst( DELAY_BLUR ) != 0 )
+        {
+            return 0;
+        }
+    }
+    
+    display_caption( "Done!" );
+    return 0;
+}
+
+int display_caption( const char* caption )
+{
+    dst = Mat::zeros( src.size(), src.type() );
+    putText(dst, caption,
+            Point(src.cols / 3, src.rows / 2),
+            FONT_HERSHEY_PLAIN, 2, Scalar(100,255, 255, 255));
+    return display_dst(DELAY_CAPTION);
+}
+
+int display_dst( int delay )
+{
+    imshow( window_name, dst );
+    int c = waitKey ( delay );
+    if( c >= 0 ) { return -1; }
+    return 0;
+}
